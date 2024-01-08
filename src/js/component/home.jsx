a49185d5ea5d30f9d2bebc09b0406c972daabfcb
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2024-01-08 20:28:59 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	function guardarCambios(event){
+		setInputValue(event.target.value)
+		
+	};
 	return (
 		<div className="text-center">
 			<h1 className="text-light fw-light fs-1 ">todos</h1>
 			<div>
-				<label for="exampleFormControlInput1" className="form-label">Email address</label>
-				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Whats needs to be done"/>
+				<form>
+				<input type="text" onChange={guardarCambios}className="form-control mx-auto shadow-sm " id="exampleFormControlInput1" placeholder="Whats needs to be done"  style={{ width: '300px' ,backgroundColor: 'white', border: 'none' , }}/>
+				</form>
 			</div>
 		</div>
 	);
